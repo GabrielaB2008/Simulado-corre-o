c7859f603e4a034fbbe34513e056554f3ba09dd3
GabrielaB2008 <gabrielabls2008@gmail.com>
2026-05-07 10:54:42 -0300

    Aula 07/05

--- app/Livewire/Movimentacao/MovimentacaoCreate.php
+++ app/Livewire/Movimentacao/MovimentacaoCreate.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Movimentacao;
 
+use App\Models\Movimentacao;
 use App\Models\Produto;
 use Livewire\Component;
 
@@ -22,5 +23,48 @@ class MovimentacaoCreate extends Component
     public function render()
     {
         return view('livewire.movimentacao.movimentacao-create');
+    }
+
+    public function store(){
+        $produto = Produto::find($this->idProdutoSelecionado);
+
+        if($produto->qtd_estoque < $this->quantidade_movimentada && $this->tipo == 'saida'){
+            $this->addError('quantidade_movimentada', 'Quantidade em estoque insuficiente');
+            return;
+        }
+
+        // Entrada de produtos
+        if($this->tipo == "entrada"){
+        // $produto->qtd_estoque = $produto->qtd_estoque + $this->quantidade_movimentada;
+           $produto->qtd_estoque += $this->quantidade_movimentada;
+        // $produto->increment('qtd_estoque', $this->quantidade);
+        } else{
+        // $produto->qtd_estoque = $produto->qtd_estoque - $this->quantidade_movimentada;
+           $produto->qtd_estoque -= $this->quantidade_movimentada;
+        // $produto->decrement('qtd_estoque', $this->quantidade_movimentada);
+        }
+
+        // Registrar Movimentação
+        Movimentacao::create([
+            'quantidade' => $this->quantidade_movimentada,
+            'data_movimentacao' => $this->data_movimentacao,
+            'tipo' => $this->tipo,
+            'produto_id' => $this->idProdutoSelecionado,
+            'user_id' => 1
+        ]);
+
+        $produto->update();
+
+        // Verificar Estoque Baixo
+        $produto->refresh();
+        if($produto->qtd_estoque < $produto->qtd_minima){
+            $this->alertaEstoqueBaixo = "ALERTA: Estoque baixo para {$produto->nome}. Quantidade atual {$produto->qtd_estoque}";
+        } else{
+            $this->alertaEstoqueBaixo = "";
+        }
+
+        session()->flash('message', 'Movimentação registrada com sucesso!');
+        $this->reset(['quantidade_movimentada', 'tipo']);
+        $this->produtos = Produto::orderby('nome')->get();
     }
 }
--- resources/views/livewire/movimentacao/movimentacao-index.blade.php
+++ resources/views/livewire/movimentacao/movimentacao-index.blade.php
@@ -1,3 +1,38 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="card">
+            <div class="card-header">
+                <h5>Movimentações</h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <div class="mb-3">
+                        <input type="text" wire:model.live='search' placeholder="Pesquisar..." class="form-control">
+                    </div>
+                    <table class="table table-stripes">
+                        <thead>
+                            <tr>
+                                <th>Tipo</th>
+                                <th>Produto</th>
+                                <th>Quantidade</th>
+                                <th>Usuário</th>
+                                <th>Data Movimentação</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($movimentacaos as $m)
+                            <tr>
+                                <td>{{ $m->tipo }}</td>
+                                <td>{{ $m->produto_id }}</td>
+                                <td>{{ $m->quantidade}}</td>
+                                <td>{{ $m->user_id }}</td>
+                                <td>{{ $m->data_movimentacao }}</td>
+                                <td><button wire:click='delete({{ $m->id }})' class="btn btn-sm btn-danger">Excluir</button></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 </div>
